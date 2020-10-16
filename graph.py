656b3411e168a025f864575deb8d1869e301b8bd
reandreev <58111850+reandreev@users.noreply.github.com>
2020-10-16 14:21:22 +0100

    Add csv

--- graph.py
+++ graph.py
@@ -1,14 +1,19 @@
 import networkx as nx
 
 
-def get_graph(file):
+def get_graph(file, file_type="txt"):
+    if file_type == "txt":
+        sep = None
+    elif file_type == "csv":
+        sep = ','
+
     G = nx.Graph()
 
     with open(file, "r", encoding="utf-8") as graph:
         lines = graph.readlines()
 
         for line in lines[4:]:
-            edge = [int(i) for i in line.split()]
+            edge = [int(i) for i in line.split(sep)]
             G.add_edge(edge[0], edge[1])
 
     return G
